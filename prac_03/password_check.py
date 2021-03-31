689aa60008317d7f53f4204a1a3c0218f018ae86
two name <https://github.com/carlton435>
2021-03-31 15:02:17 +0800

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,4 +1,17 @@
 def main():
-    password = input("Enter your password:")
+    password = get_password()
+    the_password(password)
+
+
+def the_password(password):
     print("*" * int(password))
+
+
+def get_password():
+    password = input("Enter your password:")
+    return password
+
+
 main()
+
+
