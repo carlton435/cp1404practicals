db7b01844a76d54cffdf27e79c02c8a56885f3cd
two name <https://github.com/carlton435>
2021-04-07 14:27:23 +0800

    finish the subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,6 +9,8 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    new_data = New_data()
+    print(new_data)
 
 
 def get_data():
@@ -20,9 +22,20 @@ def get_data():
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
+
+        print(parts[2])
+
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+    input_file.close()
+
+def New_data():
+    input_file = open(FILENAME)
+    for line in input_file:
+        line = line.strip()
+        parts = line.split(',')
+        print("{} is taught by {} and has {} students".format(parts[0],parts[1],parts[2]))
     input_file.close()
 
 
