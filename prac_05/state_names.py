7d752c3c9fa8e36b1f5180af0d5f00ebd35bf8a9
two name <https://github.com/carlton435>
2021-04-14 14:32:13 +0800

    complete the state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,6 +8,7 @@ File needs reformatting
 CODE_TO_NAME = {"ALICEBLUE": "#f0f8ff", "ANTIQUEWHITE": "#faebd7", "ANTIQUEWHITE1": "#ffefdb", "ANTIQUEWHITE2" : "#eedfcc", "ANTIQUEWHITE3": "#cdc0b0", "ANTIQUEWHITE4": "#8b8378", "AQUAMARINE1": "#7fffd4", "AQUAMARINE2": "#76eec6", "AQUAMARINE4": "#458b74", "AZURE1": "#f0ffff"}
 print(CODE_TO_NAME)
 
+
 state_code = input("Enter the name of color: ")
 state_code = state_code.upper()
 while state_code != "":
