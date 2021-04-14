092156f3f7afc718468d6848703f0a1c1d3fa435
two name <https://github.com/carlton435>
2021-04-14 15:28:58 +0800

    add the print("{:{}} = {}".format(x, y, z))

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,12 +1,10 @@
 Text = input("Text:")
 Text = Text.split()
-    #["this", "is", "a", "collection", "of", "words", "of", "nice", "words", "this", "is", "a", "fun", "thing", "it", "is"]
-print("Text: this is a collection of words of nice words this is a fun thing it is")
 word_to_count = {}
 for word in Text:
     try:
         word_to_count[word] += 1
     except KeyError:
         word_to_count[word] = 1
-print(word_to_count)
+print("{:{}} = {}".format(0, 1, word_to_count))
 
